b13afc60247cee0b06eed980c736559a9857e8e4
cellistigs <ta2507@columbia.edu>
2020-08-24 12:30:23 -0400

    testing updates on markdown.

--- docs/meeting_reports/update_figs.py
+++ docs/meeting_reports/update_figs.py
@@ -6,7 +6,16 @@ if __name__ == "__main__":
     experimentname = "TempTrial2"
     roi = 1
     p = 0
-    interval = [70567, 70630]
+    interval = [41435, 41466]
     filtername = "velocity_20_distance_30_filter"
     plotpath = "./images/example_trace1.png"
     tracedata.plot_trajectory(experimentname,roi,p,interval,filtername,plotpath)
+
+    ## Check one thing
+    experimentname = "TempTrial17"
+    roi = 2
+    p = 7
+    interval = [46058, 46092]
+    filtername = "velocity_20_distance_30_filter"
+    tracedata = tracedata.load_trace_data(experimentname,roi,p,interval,filtername)
+    print(tracedata["pursuit_direction"])
