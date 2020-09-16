49d715917a25de9f22c9563a7df739421eb6cd24
cellistigs <ta2507@columbia.edu>
2020-09-16 01:09:13 -0400

    added some toy cases of straight lines, having some weird plotting but intuition abot symmetric parts looks good.

--- scripts/explore_freq_methods.py
+++ scripts/explore_freq_methods.py
@@ -1,5 +1,6 @@
 ### Script to explore frequency and time-frequency methods for the analysis of this data. 
 import os
+import numpy as np
 from script_doc_utils import initialize_doc,insert_image,get_relative_image_path
 from social_pursuit import data, fft
 from mdutils.mdutils import MdUtils
@@ -36,7 +37,13 @@ if __name__ == "__main__":
     analysisobj.plot_spectrum(trace,image2_path)
     image2_rel_path = get_relative_image_path(image2_path)
     insert_image(md,image2_rel_path,align = 'center')
-    md.new_paragraph("This representation shows that there is a sharp peak in the frequency content of this trajectory in frequencies close to zero. This means that relatively slow frequencies dominate the activity that we see, setting us up to reduce dimensionality by considering only the low frequency activity in further analyses. ")
+    md.new_paragraph("This representation shows that there is a sharp peak in the frequency content of this trajectory in frequencies close to zero. This means that relatively slow frequencies dominate the activity that we see, setting us up to reduce dimensionality by considering only the low frequency activity in further analyses. Furthermore, the existence of positive and negative frequencies indicate components that rotate in opposing directions. Symmetry between the positive and negative frequencies should (I think) indicate straight trajectories, or trajectories with an equal and opposing amount of rotation to the left vs. the right.")
+    ## straight diagonal
+    #straighttrace = np.concatenate([np.linspace(0,1,10)[:,None],np.linspace(-1,0,10)[:,None]],axis =1)
+    ## straight y only
+    straighttrace = np.concatenate([np.linspace(0,1,10)[:,None],np.linspace(0,0,10)[:,None]],axis =1)
+    print(straighttrace)
+    analysisobj.plot_spectrum(straighttrace,"../docs/script_docs/images/straighttracespectrum.png")
     md.new_paragraph("We can also apply an inverse fourier transform to reconstruct the data back from this frequency representation:")
     image3_path = "../docs/script_docs/images/candidate_pursuit_dam_full_reconstruct.png"
     analysisobj.plot_compare_trace_reconstruct(trace,image3_path)
@@ -44,6 +51,7 @@ if __name__ == "__main__":
     insert_image(md,image3_rel_path,align = 'center')
 
     ## Plot a rotated and translated version of that test trace and see how that fares under same analytics.  
+    md.new_paragraph("One important thing we want to do with these datasets now is to cluster them. In order to do so, we should figure out which invariances we care about. For example, by disregarding the 0th component of the fourier transform, we can translate all trajectories back to be centered at the origin, allowing for comparison only of the morphologies of the resulting trajectories. ")
 
     ## Plot a corrupted version with a point discontinuity. Can we distinguish this in the fourier spectrum?   
     ## What about over different corruptions (create a bootstrap estimate)
